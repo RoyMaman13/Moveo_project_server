0cde2afa814bab6717c1d2b950e4e6d0477175a0
roy maman <roey.maman@gmail.com>
2023-11-29 17:44:38 +0200

    a

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -21,6 +21,6 @@ app.use('/', routes); // Use the routes module
 setupSocket(server);
 
 // Server listen
-server.listen(4000 || process.env.SERVER_PORT, "0.0.0.0",() => {
+server.listen(4000 || process.env.PORT, "0.0.0.0",() => {
   console.log(`Server is running on port ${PORT}`);
 });
